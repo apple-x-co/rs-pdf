40c67af424cbe0ed7ddf55f9bdd90a6e1d9642d8
Kouhei.Sano <apple.x.co@gmail.com>
2025-02-04 08:54:31 +0900

    Add text

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use printpdf::{PdfDocument, Mm};
+use printpdf::{PdfDocument, Mm, BuiltinFont};
 use std::fs::File;
 use std::io::BufWriter;
 
 fn main() {
-    let (doc, _, _) = PdfDocument::new("Title", Mm(210.0), Mm(297.0), "Layer");
+    let (doc, page_index, layer_index) = PdfDocument::new("Title", Mm(210.0), Mm(297.0), "Layer");
+
+    let layer = doc.get_page(page_index).get_layer(layer_index);
+
+    let font = doc.add_builtin_font(BuiltinFont::HelveticaBold).unwrap();
+    layer.use_text("HELLO WORLD", 48.0, Mm(10.0), Mm(20.0), &font);
+
     doc.save(&mut BufWriter::new(File::create("output/printpdf_output.pdf").unwrap()))
         .unwrap();
 }
