99695499d15600f9958ec46c49e78ccb8e765d77
Kouhei.Sano <apple.x.co@gmail.com>
2025-02-04 08:38:54 +0900

    Generate blank pdf

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use printpdf::{PdfDocument, Mm};
+use std::fs::File;
+use std::io::BufWriter;
+
 fn main() {
-    println!("Hello, world!");
+    let (doc, _, _) = PdfDocument::new("Title", Mm(210.0), Mm(297.0), "Layer");
+    doc.save(&mut BufWriter::new(File::create("output/printpdf_output.pdf").unwrap()))
+        .unwrap();
 }
