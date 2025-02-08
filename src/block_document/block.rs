4e837bfff0d6dcb65061f3cb3cccfae07c85a209
Kouhei.Sano <apple.x.co@gmail.com>
2025-02-08 15:18:15 +0900

    Add block struct

--- src/block_document/block.rs
+++ src/block_document/block.rs
@@ -1,7 +1,32 @@
-pub struct Block {}
+pub enum Direction {
+    Horizontal,
+    Vertical,
+}
+
+pub struct Size {
+    pub width: f64,
+    pub height: f64,
+}
+
+pub struct Coordinate {
+    pub x: u64,
+    pub y: u64,
+}
+
+pub struct Block {
+    blocks: Vec<Block>,
+    direction: Direction,
+    size: Option<Size>,
+    coordinate: Option<Coordinate>,
+}
 
 impl Block {
     pub fn new() -> Block {
-        Block {}
+        Block {
+            blocks: Vec::new(),
+            direction: Direction::Horizontal,
+            size: None,
+            coordinate: None,
+        }
     }
 }
