966549d1bba09e33b38d3b36ab35ebd7f6749909
Kouhei.Sano <apple.x.co@gmail.com>
2025-02-17 08:48:09 +0900

    Chore

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,9 @@ use block_document::pdf_writer;
 use std::fs::File;
 
 fn main() {
-    // let document1 = document_json::parse(); // TODO: 実行時パラメータの JSON ファイル名を渡す
-    // pdf_writer::dummy(
-    //     document1,
-    //     File::create("output/printpdf_output_.pdf").unwrap(),
-    // );
-
-    let document2 = document_json::parse(); // TODO: 実行時パラメータの JSON ファイル名を渡す
+    let document = document_json::parse(); // TODO: 実行時パラメータの JSON ファイル名を渡す
     pdf_writer::save(
-        document2,
+        document,
         File::create("output/printpdf_output.pdf").unwrap(),
     ); // TODO: 実行時パラメータの 出力ファイル名 を渡す
 }
