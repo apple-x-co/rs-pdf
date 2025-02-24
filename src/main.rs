6e7c9eb95951c22a20334236bd4b5d84938169e8
Kouhei.Sano <apple.x.co@gmail.com>
2025-02-25 08:52:45 +0900

    refactor:

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,30 @@ mod block_document;
 use block_document::document_json;
 use block_document::pdf_writer;
 use std::fs::File;
+use std::path::Path;
+use std::process::exit;
 
 fn main() {
+    let output_path = "output/printpdf_output.pdf"; // TODO: 実行時パラメータの 出力ファイル名 を渡す
+    let is_debug = true; // TODO: 実行時パラメーターの デバッグ を渡す
+
+    if Path::new(output_path).exists() {
+        eprintln!("The output path already exists!");
+        exit(1);
+    }
+
+    let output_file = File::create(&output_path).map_err(|e|{
+        eprintln!("Could not create output file! {}", e);
+        e
+    });
+    let file = match output_file {
+        Ok(file) => file,
+        Err(e) => {
+            eprintln!("Fatal error: {}", e);
+            exit(1);
+        }
+    };
+
     let document = document_json::parse(); // TODO: 実行時パラメータの JSON ファイル名を渡す
-    pdf_writer::save(
-        document,
-        File::create("output/printpdf_output.pdf").unwrap(),
-        true,
-    ); // TODO: 実行時パラメータの 出力ファイル名 を渡す
+    pdf_writer::save(document, file, is_debug);
 }
