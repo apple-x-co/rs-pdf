befa8ea48e8c6ce4d2607f8618a4adc62f58db94
Kouhei.Sano <apple.x.co@gmail.com>
2025-02-24 09:19:54 +0900

    chore:

--- src/block_document/block.rs
+++ src/block_document/block.rs
@@ -3,7 +3,6 @@ use crate::block_document::image::Image;
 use crate::block_document::line::Line;
 use crate::block_document::rectangle::Rectangle;
 use crate::block_document::text::Text;
-use std::any::TypeId;
 
 #[derive(Debug, Clone)]
 pub enum BlockType {
@@ -13,17 +12,3 @@ pub enum BlockType {
     Text(Text),
     Image(Image),
 }
-
-// impl Block for BlockType {}
-
-// impl BlockType {
-//     fn type_id(&self) -> TypeId {
-//         match self {
-//             BlockType::Container(_) => TypeId::of::<BlockContainer>(),
-//             BlockType::Line(_) => TypeId::of::<Line>(),
-//             BlockType::Rectangle(_) => TypeId::of::<Rectangle>(),
-//             BlockType::Text(_) => TypeId::of::<Text>(),
-//             BlockType::Image(_) => TypeId::of::<Image>(),
-//         }
-//     }
-// }
